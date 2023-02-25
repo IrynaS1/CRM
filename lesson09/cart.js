8d3f8c7d651242a47fd8527389c5b9babe6b2359
IrynaS1 <snowqueen-2007@mail.ru>
2023-02-25 21:45:13 +0300

    changed cart.js

--- lesson09/cart.js
+++ lesson09/cart.js
@@ -2,21 +2,16 @@
 
 const cart = {
 	items: [],
-	totalPrice: 0,
 	count: 0,
-	getTotalPrice() {
-		return this.totalPrice;
+	get totalPrice() {
+		return 4;
 	},
 	add: function (nameOfGood, priceOfGood, countOfGood = 1) {
-		const good = {
+		this.items.push(this.items.good = {
 			nameOfGood,
 			priceOfGood,
 			countOfGood,
-		};
-
-		this.items.push(good);
-		this.calculateItemPrice();
-		this.increaseCount();
+		});
 	},
 	increaseCount: function () {
 		for (let keyOne in this.items) {
@@ -25,6 +20,17 @@ const cart = {
 		this.count = this.count + countOfGoods;
 	},
 	calculateItemPrice: function () {
+		/* 
+				let priceOfGoods = 0;
+				priceOfGoods = this.items.map((item) => {
+					item = this.items.item.priceOfGood;
+					console.log('элемент item', item);
+				});
+				console.log('элемент priceOfGoods', priceOfGoods); */
+
+		/* let priceOfGoods = this.items.reduce((acc, b) => acc + this.items[key].priceOfGood, 0);
+		console.log('общая priceOfGoods', priceOfGoods); */
+
 		let priceOfGoods = 0;
 		for (let key in this.items) {
 			priceOfGoods = this.items[key].priceOfGood;
@@ -35,19 +41,22 @@ const cart = {
 			countOfGoods = this.items[keyOne].countOfGood;
 		}
 
-		this.totalPrice = this.totalPrice + priceOfGoods * countOfGoods;
+		let totalPPrice = 0;
+		totalPPrice = totalPPrice + priceOfGoods * countOfGoods;
+		return totalPPrice;
+		console.log('общее значение totalPPrice', totalPPrice);
 	},
 	clear() {
 		this.items = Object.create(null);
 	},
 	print() {
 		console.log(JSON.stringify(this.items));
-		console.log(this.totalPrice);
+		//console.log(this.totalPrice);
 		console.log(this.count);
 	},
 }
 
-cart.add('cucumber', 152, 2);
+cart.add('cucumber', 150, 2);
 cart.add('salo', 300, 3);
 cart.add('bread', 180, 1);
 cart.print();
